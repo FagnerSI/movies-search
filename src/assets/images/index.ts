b58578d8d8f82b3274a11bfe8d27e6184bc0a150
Fagner Pinheiro <fagner314@gmail.com>
2024-09-08 11:35:58 -0300

    feat(assests): adiciona imagens com versoes do logo da aplicacao

---
+++ src/assets/images/index.ts
@@ -0,0 +1,3 @@
+export const logo = require('../images/SearchMoves.png');
+export const logo2x = require('../images/SearchMoves@2x.png');
+export const logo3x = require('../images/SearchMoves@3x.png')

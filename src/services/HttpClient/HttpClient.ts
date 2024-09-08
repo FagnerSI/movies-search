6eb3b8a6b160e15b40076ee3e55e04f319bb50b2
Fagner Pinheiro <fagner314@gmail.com>
2024-09-08 11:26:19 -0300

    feat(services): adiciona cliente http

---
+++ src/services/HttpClient/HttpClient.ts
@@ -0,0 +1,66 @@
+import { IHttpClient, IHttpClientRequest, THttpData, THttpHeaders, THttpURL } from "./types";
+
+export class HttpClient implements IHttpClient {
+    baseUrl = ''; 
+    queryKeyParams = '';
+    queryParams = '';
+
+    constructor(url = '', queryParams?: string) {
+        this.baseUrl = url;
+        this.queryKeyParams = queryParams || '';
+    }
+
+    async request({ method, url, data, headers = {} }: IHttpClientRequest) {
+        const options: RequestInit = {
+            method,
+            headers: {
+                'Content-Type': 'application/json',
+                ...headers,
+            },
+            body: data ? JSON.stringify(data) : null,
+        };
+
+        try {
+            const finalyUrl = this.baseUrl + url + '?' + [this.queryParams, this.queryKeyParams].join('&')
+            const response = await fetch(finalyUrl, options);
+            
+            if (!response.ok) {
+                const errorText = await response.text();
+                throw new Error(`HTTP Error ${response.status}: ${errorText}`);
+            }
+            const responseData = await response.json();
+            return responseData;
+        } catch (error) {
+           // console.error('HTTP Request Error:', error);
+            throw error;
+        }
+    }
+
+    setQueryParams = (query?: string) => {
+        if(query){
+            this.queryParams = query;
+        }
+    }
+
+    get(url: THttpURL, query?: string, headers: THttpHeaders = {}) {
+        this.setQueryParams(query)
+        
+        return this.request({ method: 'GET', url, headers});
+    }
+
+    delete(url: THttpURL, headers: THttpHeaders = {}) {
+        return this.request({ method: 'DELETE', url, headers});
+    }
+
+    post(url: THttpURL, data: THttpData, headers: THttpHeaders = {}) {
+        return this.request({ method:'POST', url, data, headers});
+    }
+
+    put(url: THttpURL, data: THttpData, headers: THttpHeaders = {}) {
+        return this.request({ method: 'PUT', url, data, headers});
+    }
+
+   
+}
+
+
---
+++ src/services/HttpClient/types.ts
@@ -0,0 +1,21 @@
+export type THttpURL = string;
+export type THttpHeaders = RequestInit['headers'];
+export type THttpData = Object;
+export type THttpMethod = 'GET'| 'POST' | 'DELETE' |'PUT';
+
+export interface IHttpClientRequest {
+    url: THttpURL,
+    method: THttpMethod,
+    data?: THttpData,
+    headers?: THttpHeaders
+}
+
+export interface IHttpClient {
+    baseUrl: THttpURL;
+    get: (url: THttpURL, query?: string, headers?: THttpHeaders) => Promise<any>;
+    delete: (url: THttpURL, headers?: THttpHeaders) => Promise<any>;
+    post: (url: THttpURL, data: THttpData, headers?: THttpHeaders) => Promise<any>;
+    put: (url: THttpURL, data: THttpData, headers?: THttpHeaders) => Promise<any>;
+    request: (params: IHttpClientRequest)  => Promise<any>;
+    setQueryParams:(query: string) => void;
+}
